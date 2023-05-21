35f0ce17cc99650606b4abfba2350e2d16e54dbf
craig johnson <cj.craigajohnson@gmail.com>
2023-05-21 15:47:16 -0400

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const thanks = ['Guadalupe', 'Ollie', 'Aki' ];
+
+function writeCards(thanks) {
+let birthday = []
+
+  for (let i = 0; i < thanks.length; i++) {
+    birthday.push(`Thank you, ${thanks[i]}, for the wonderful surprise gift!`);
+console.log(birthday);
+  }
+
+  return birthday;
+}
+
+writeCards(thanks);
+
+function countDown(number) {
+while (0 <= number) {
+  console.log(number--);
+}
+}
